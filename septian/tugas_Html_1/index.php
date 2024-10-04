2d21f219456a9fad290518cb70e98c2696ce9fc2
jokerredsoil <septianxnugraha@gmail.com>
2024-10-04 13:32:28 +0700

    pindah web sebelumnya ke folder latihan 1, buat folder tugas html 1 content: index,daftarmenu,data penjualan,form pendaftaran, galler

---
+++ septian/tugas_Html_1/index.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas HTML 1</title>
+</head>
+<body>
+    <header>
+        <h1>Selamat Datang Di Halaman Utama</h1>
+        <nav>
+
+            <a href="gallery.php">| Gallery |</a>
+            <a href="dataPenjualan.php"> Data Penjualan |</a>
+            <a href="formPendaftaran.php">|Form Pendaftaran |</a>
+            <a href="daftarMenu.php"> Daftar Menu |</a>
+            
+        </nav>
+    </header>
+    
+</body>
+</html>
