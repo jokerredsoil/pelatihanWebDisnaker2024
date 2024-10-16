4cd8c39901cba797bee6d8303a1e1eaae67726a6
jokerredsoil <septianxnugraha@gmail.com>
2024-10-16 16:00:12 +0700

    add folder belajar php

---
+++ septian/belajarPHP/index.php
@@ -0,0 +1,25 @@
+<?php
+    
+    // echo untuk menampilkan string
+    // gunakan \n untuk baris baru di terminal
+    // gunakan echo <br> untuk baris baru di html
+    echo "hello world! \n ";
+
+    print("hello world \n ");
+    
+    print_r("hello world \n");
+
+    var_dump("hahahaha \n");
+
+    echo "string gatai " . " henshin!!!! \n ";
+
+    var_dump("string gatai " . " henshin!!!!");
+
+    $x = 9;
+    $y = 2;
+    // var_dump($Y);
+    $z = $x % $y;
+
+    echo "$x" . "/" . "$y". "= $z"
+
+?>
