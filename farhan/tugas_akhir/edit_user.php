16933156e15e9ad1088cc224811cd8d9e72fe328
AANG-Enk <farhanmohaemin14@gmail.com>
2024-10-21 16:37:25 +0700

    set view form user

--- farhan/tugas_akhir/edit_user.php
+++ farhan/tugas_akhir/edit_user.php
@@ -1,6 +1,8 @@
 <?php 
     require_once "koneksi.php";
     $id = $_GET['id'];
+    $res = mysqli_query($koneksi,"SELECT * FROM user WHERE id = $id");
+    $data = mysqli_fetch_array($res);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,15 +17,15 @@
     <form action="./aksi/tambah_user.php" method="post">
         <div>
             <label for="nama">Nama</label>
-            <input type="text" name="nama" id="nama" placeholder="Masukkan Nama">
+            <input type="text" name="nama" id="nama" value="<?=$data['name']?>" placeholder="Masukkan Nama">
         </div>
         <div>
             <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Masukkan Username">
+            <input type="text" name="username" id="username" value="<?=$data['username']?>" placeholder="Masukkan Username">
         </div>
         <div>
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Masukkan Email">
+            <input type="email" name="email" id="email" value="<?=$data['email']?>" placeholder="Masukkan Email">
         </div>
         <div>
             <label for="password">Password</label>
@@ -32,10 +34,10 @@
         <div>
             <label for="">Role</label>
             <label for="admin">
-                <input type="radio" name="role" id="admin" value="Admin"> Admin
+                <input type="radio" name="role" id="admin" value="Admin" <?= $data['role'] == 'Admin' ? 'checked' : '' ?>> Admin
             </label>
             <label for="user">
-                <input type="radio" name="role" id="user" value="User"> User
+                <input type="radio" name="role" id="user" value="User" <?= $data['role'] == 'User' ? 'checked' : '' ?>> User
             </label>
         </div>
         <button type="submit">Simpan</button>
