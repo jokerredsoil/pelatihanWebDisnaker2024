84c0b0b389e33a404b553bc0737c4cb75ef11ae8
AANG-Enk <farhanmohaemin14@gmail.com>
2024-10-22 13:13:50 +0700

    crud suplier with style done

--- farhan/tugas_akhir/form_suplier.php
+++ farhan/tugas_akhir/form_suplier.php
@@ -1,30 +1,49 @@
 <?php 
-    require_once "koneksi.php";
+    require "koneksi.php";
+    include "shared/header.php";
+    include "shared/side.php";
+    $link = './aksi/tambah_suplier.php';
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $res_suplier = mysqli_query($koneksi,"SELECT * FROM suplier WHERE id = $id");
+        $data_old = mysqli_fetch_array($res_suplier);
+        $link = './aksi/edit_suplier.php';
+    }    
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Suplier</title>
-</head>
-<body>
-    <h1>Tambah Suplier</h1>
-    <a href="./">Kembali</a>
-    <form action="./aksi/tambah_suplier.php" method="post">
-        <div>
-            <label for="nama">Nama Suplier</label>
-            <input type="text" name="nama" id="nama" placeholder="Masukkan Nama Suplier">
+<div class="col-sm-12 col-md-9">
+    <div class="card">
+        <div class="card-header">
+            <h4>Tambah Suplier</h4>
         </div>
-        <div>
-            <label for="kontak">Kontak Suplier</label>
-            <input type="number" name="kontak" id="kontak" placeholder="Masukkan Kontak Suplier">
-        </div>
-        <div>
-            <label for="alamat">Alamat</label>
-            <textarea name="alamat" id="alamat" placeholder="Masukkan Alamat Suplier"></textarea>
-        </div>
-        <button type="submit">Simpan</button>
-    </form>
-</body>
-</html>
+        <form action="<?=$link?>" class="row g-3 needs-validation" novalidate method="post">
+            <?php if(isset($data_old)) {?>
+                <input type="hidden" name="suplier" value="<?=$id?>">
+            <?php }?>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <div class="mb-3">
+                            <label class="form-label" for="nama">Nama Suplier</label>
+                            <input class="form-control" type="text" name="nama" id="nama" value="<?= isset($data_old) ? $data_old['nama'] : '' ?>" placeholder="Masukkan Nama Suplier">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="kontak">Kontak Suplier</label>
+                            <input class="form-control" type="number" name="kontak" id="kontak" value="<?= isset($data_old) ? $data_old['kontak'] : '' ?>" placeholder="Masukkan Kontak Suplier">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="alamat">Alamat</label>
+                            <textarea class="form-control" name="alamat" id="alamat" placeholder="Masukkan Alamat Suplier"><?= isset($data_old) ? $data_old['alamat'] : '' ?></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer d-flex justify-content-end gap-3">
+                <a href="./suplier.php" class="btn btn-danger">Kembali</a>
+                <button type="submit" class="btn btn-success">Simpan</button>
+            </div>
+        </form>
+    </div>
+</div>
+<?php 
+    include "shared/footer.php"
+?>
--- farhan/tugas_akhir/suplier.php
+++ farhan/tugas_akhir/suplier.php
@@ -1,63 +1,69 @@
 <?php 
-    require_once "koneksi.php";
+    require "koneksi.php";
+    include "shared/header.php";
+    include "shared/side.php";
     $res = mysqli_query($koneksi, "SELECT * FROM suplier WHERE deleted_at IS NULL");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-</head>
-<body>
-    <h1>Data Suplier</h1>
-    <div>
-        <a href="./suplier.php">Suplier</a>
-        <a href="./">Barang</a>
-        <a href="./barang_masuk.php">Log Masuk</a>
-        <a href="./barang_keluar.php">Log Keluar</a>
+<div class="col-sm-12 col-md-9">
+    <div class="card">
+        <div class="card-header">
+            <h4>Daftar Barang</h4>
+        </div>
+        <div class="card-body">
+            <?php if(isset($_GET['pesan'])){
+                ?>
+                    <div class="alert alert-light" role="alert">
+                        <?= str_replace('-',' ',$_GET['pesan']) ?>
+                    </div>
+                <?php
+            } ?>
+            <div class="table-responsive">
+                <table class="table table-bordered table-datatables">
+                    <thead>
+                        <tr>
+                            <th class="text-center">No</th>
+                            <th class="text-center">Nama</th>
+                            <th class="text-center">Kontak</th>
+                            <th class="text-center">Alamat</th>
+                            <?php if(isset($_SESSION['role']) && $_SESSION['role'] == 'Admin') {?>
+                                <th class="text-center">Aksi</th>
+                            <?php }?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        $no = 1;
+                            while($d = mysqli_fetch_array($res)){
+                        ?>
+                            <tr>
+                                <td class="text-center"><?= $no ?></td>
+                                <td><?= $d['nama'] ?></td>
+                                <td><?= $d['kontak'] ?></td>
+                                <td><?= $d['alamat'] ?></td>
+                                <?php if(isset($_SESSION['role']) && $_SESSION['role'] == 'Admin') {?>
+                                    <td>
+                                        <div class="d-flex gap-2 justify-content-center">
+                                            <a href="./form_suplier.php?id=<?=$d['id']?>" class="btn btn-warning">Edit</a>
+                                            <a href="./aksi/hapus_suplier.php?id=<?=$d['id']?>" class="btn btn-danger">Hapus</a>
+                                        </div>
+                                    </td>
+                                <?php }?>
+                            </tr>
+                        <?php
+                                $no++;
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
         <?php if(isset($_SESSION['role']) && $_SESSION['role'] == 'Admin') {?>
-            <a href="./user.php">User Management</a>
+            <div class="card-footer d-flex justify-content-center">
+                <a href="./form_suplier.php" class="btn btn-primary">Tambah Data</a>
+            </div>
         <?php }?>
-        <a href="./aksi/auth/logout.php">Logout</a>
     </div>
-    <a href="./form_suplier.php">Tambah Data</a>
-    <?php if(isset($_GET['pesan'])){
-        echo "<span><strong>".str_replace('-',' ',$_GET['pesan'])."</strong></span>";
-    }?>
-    <table border=1>
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Kontak</th>
-                <th>Alamat</th>
-                <?php if(isset($_SESSION['role']) && $_SESSION['role'] == 'Admin') {?>
-                    <th>Aksi</th>
-                <?php }?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                $no = 1;
-                while($d = mysqli_fetch_array($res)){
-            ?>
-                <tr>
-                    <td><?= $no ?></td>
-                    <td><?= $d['nama'] ?></td>
-                    <td><?= $d['kontak'] ?></td>
-                    <td><?= $d['alamat'] ?></td>
-                    <?php if(isset($_SESSION['role']) && $_SESSION['role'] == 'Admin') {?>
-                        <td>
-                            <a href="./edit_suplier.php?id=<?=$d['id']?>">Edit</a>|<a href="./aksi/hapus_suplier.php?id=<?=$d['id']?>">Hapus</a>
-                        </td>
-                    <?php }?>
-                </tr>
-            <?php
-                    $no++;
-                }
-            ?>
-        </tbody>
-    </table>
-</body>
-</html>
+</div>
+<?php 
+    include "shared/footer.php"
+?>
