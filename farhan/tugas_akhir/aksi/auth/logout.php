9527e4ca8c87112714c686d946d72dd5c8daa2df
AANG-Enk <farhanmohaemin14@gmail.com>
2024-10-22 14:11:16 +0700

    hapus koneksi login dan buat login baruy

--- farhan/tugas_akhir/aksi/auth/logout.php
+++ farhan/tugas_akhir/aksi/auth/logout.php
@@ -1,5 +1,16 @@
 <?php
-require_once "../../koneksi_login.php";
+session_start();
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "db_gudang";
+
+$koneksi = mysqli_connect($hostname,$username,$password,$database);
+
+if(mysqli_connect_errno()){
+    echo "Failed to connect databse : ".mysqli_connect_error();
+    exit();
+}
 
 session_destroy();
 
