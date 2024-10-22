0b3ee4da239d44284ff29a1cfd3bb04126aba47f
AANG-Enk <farhanmohaemin14@gmail.com>
2024-10-22 13:52:56 +0700

    crud user with style done

--- farhan/tugas_akhir/form_user.php
+++ farhan/tugas_akhir/form_user.php
@@ -1,43 +1,67 @@
 <?php 
-    require_once "koneksi.php";
+    require "koneksi.php";
+    include "shared/header.php";
+    include "shared/side.php";
+    $link = './aksi/tambah_user.php';
+    $res = mysqli_query($koneksi, "SELECT * FROM barang WHERE deleted_at IS NULL");
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $res_user = mysqli_query($koneksi,"SELECT * FROM user WHERE id = $id");
+        $data_old = mysqli_fetch_array($res_user);
+        $link = './aksi/edit_user.php';
+    }    
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah User</title>
-</head>
-<body>
-    <h1>Tambah User</h1>
-    <a href="./">Kembali</a>
-    <form action="./aksi/tambah_user.php" method="post">
-        <div>
-            <label for="nama">Nama</label>
-            <input type="text" name="nama" id="nama" placeholder="Masukkan Nama">
+<div class="col-sm-12 col-md-9">
+    <div class="card">
+        <div class="card-header">
+            <h4>Tambah Pengguna Baru</h4>
         </div>
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Masukkan Username">
-        </div>
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Masukkan Email">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Masukkan Password">
-        </div>
-        <div>
-            <label for="">Role</label>
-            <label for="admin">
-                <input type="radio" name="role" id="admin" value="Admin"> Admin
-            </label>
-            <label for="user">
-                <input type="radio" name="role" id="user" value="User"> User
-            </label>
-        </div>
-        <button type="submit">Simpan</button>
-    </form>
-</body>
-</html>
+        <form action="<?=$link?>" class="row g-3 needs-validation" novalidate method="post">
+            <?php if(isset($data_old)) {?>
+                <input type="hidden" name="user" value="<?=$id?>">
+            <?php }?>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <div class="mb-3">
+                            <label class="form-label" for="nama">Nama</label>
+                            <input class="form-control" type="text" name="nama" id="nama" value="<?= isset($data_old) ? $data_old['name'] : '' ?>" placeholder="Masukkan Nama">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="username">Username</label>
+                            <input class="form-control" type="text" name="username" id="username" value="<?= isset($data_old) ? $data_old['username'] : '' ?>" placeholder="Masukkan Username">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="email">Email</label>
+                            <input class="form-control" type="email" name="email" id="email" value="<?= isset($data_old) ? $data_old['email'] : '' ?>" placeholder="Masukkan Email">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="password">Password</label>
+                            <input class="form-control" type="password" name="password" id="password" placeholder="Masukkan Password">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="">Role</label> <br>
+                            <div class="form-check">
+                                <label class="form-check-label" for="admin">
+                                    <input class="form-check-input" type="radio" name="role" id="admin" value="Admin" <?= (isset($data_old) && $data_old['role'] == "Admin") ? "checked" : '' ?>> Admin
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <label class="form-check-label" for="user">
+                                    <input class="form-check-input" type="radio" name="role" id="user" value="User" <?= (isset($data_old) && $data_old['role'] == "User") ? "checked" : '' ?> > User
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer d-flex justify-content-end gap-3">
+                <a href="./user.php" class="btn btn-danger">Kembali</a>
+                <button type="submit" class="btn btn-success">Simpan</button>
+            </div>
+        </form>
+    </div>
+</div>
+<?php 
+    include "shared/footer.php"
+?>
